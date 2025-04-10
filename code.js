c0b06fe3ec3455ab95d77f4a2c4355ce1c4029cc
dereckc210 <dereck.pirulin@icloud.com>
2025-04-10 13:21:17 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
+document.getElementById("checkButton").addEventListener("click", function() {
+  let age = document.getElememtById("age").value;
+  let resultText = document.getElementById("resultText")
+  let resultImage = document.getElementById("resultImage")
 
+if(age 17) {
+resultText.innerText = "resultText";
+resultText.style.color="green";
+resultImage.src = "YOUR";
+} else if (age 16 && 16){
+  resultText.innerText = "";
+  resultText.style.color = "";
+resultImage.src = "YOUR CAR";
