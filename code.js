9e4909735d36caab0298e361e78fb71036cb3155
dereckc210 <dereck.pirulin@icloud.com>
2025-04-11 13:04:00 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 document.getElementById("checkButton").addEventListener("click", function() {
-  let age = document.getElementById("age").value;
+  let age = document.getElementById("ageInput").value;
   let resultText = document.getElementById("resultText")
   let resultImage = document.getElementById("resultImage")
 
