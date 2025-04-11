88e7dd23e8e8afd09c3503264052d50d8d1ecdf7
dereckc210 <dereck.pirulin@icloud.com>
2025-04-11 12:47:32 -0400

    Update code.js

--- code.js
+++ code.js
@@ -8,12 +8,12 @@ resultText.innerText = "resultText";
 resultText.style.color="green";
 resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbicyclingaustralia.com.au%2Fbikes%2Fridden-reviewed-world-bicycle-reliefs-buffalo-bike%2F&psig=AOvVaw3p26N_ZWYqOH7Uw72UxJQk&ust=1744475932167000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjWkMK10IwDFQAAAAAdAAAAABAE";
 } else if (age >= 16 && age <= 24){
-  resultText.innerText = "";
-  resultText.style.color = "";
+  resultText.innerText = "resultText";
+  resultText.style.color = "green";
 resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kbb.com%2Fwhat-is%2Fluxury-car%2F&psig=AOvVaw3gcuAYUVGYoCQLC9aAUuMZ&ust=1744476158785000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDyhKW20IwDFQAAAAAdAAAAABAJ";
 } else if (age >= 25) {
-resultText.innerText = "text";
-resultText.style.color = "red";
+resultText.innerText = "resultText";
+resultText.style.color = "green";
 resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdrivo.com%2Fabout%2Flocations%2FEWR&psig=AOvVaw2_te9iiSuJagNvDghwqB9S&ust=1744476195243000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCf1LG20IwDFQAAAAAdAAAAABAI";
 }
 });
